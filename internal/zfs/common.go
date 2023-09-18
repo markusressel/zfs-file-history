b0a999c4c605e133efe5d9268b7b3399a6e80d06
Markus Ressel <mail@markusressel.de>
2023-09-18 19:56:56 +0200

    refactoring

---
+++ internal/zfs/common.go
@@ -0,0 +1,73 @@
+package zfs
+
+import (
+	golibzfs "github.com/bicomsystems/go-libzfs"
+	"strings"
+	"zfs-file-history/internal/logging"
+)
+
+var (
+	allDatasets  = []golibzfs.Dataset{}
+	AllSnapshots = map[string][]golibzfs.Dataset{}
+)
+
+func init() {
+	RefreshZfsData()
+}
+
+func RefreshZfsData() {
+	loadDatasets()
+	loadSnapshots(allDatasets)
+}
+
+func loadDatasets() {
+	datasets, err := golibzfs.DatasetOpenAll()
+	if err != nil {
+		logging.Error(err.Error())
+	} else {
+		allDatasets = datasets
+	}
+}
+
+func loadSnapshots(datasets []golibzfs.Dataset) {
+	for _, dataset := range datasets {
+		nameProperty := dataset.Properties[golibzfs.DatasetPropName]
+		//mountPointProperty := dataset.Properties[golibzfs.DatasetPropMountpoint]
+		snapshots, err := dataset.Snapshots()
+		if err != nil {
+			logging.Error(err.Error())
+		} else {
+			AllSnapshots[nameProperty.Value] = snapshots
+		}
+		loadSnapshots(dataset.Children)
+	}
+}
+
+func findDataset(datasets []golibzfs.Dataset, path string) *golibzfs.Dataset {
+	for _, dataset := range datasets {
+		mountPoint := dataset.Properties[golibzfs.DatasetPropMountpoint]
+		if mountPoint.Value == path {
+			return &dataset
+		}
+		dataset := findDataset(dataset.Children, path)
+		if dataset != nil {
+			return dataset
+		}
+	}
+	return nil
+}
+
+func findSnapshot(snapshots []golibzfs.Dataset, name string) *golibzfs.Dataset {
+	for _, snapshot := range snapshots {
+		nameProperty := snapshot.Properties[golibzfs.DatasetPropName]
+		currentName := strings.Split(nameProperty.Value, "@")[1]
+		if currentName == name {
+			return &snapshot
+		}
+		snapshot := findSnapshot(snapshot.Children, name)
+		if snapshot != nil {
+			return snapshot
+		}
+	}
+	return nil
+}
--- internal/zfs/dataset.go
+++ internal/zfs/dataset.go
@@ -8,69 +8,16 @@ import (
 	"os"
 	path2 "path"
 	"strconv"
-	"strings"
 	"time"
 	"zfs-file-history/internal/logging"
 	"zfs-file-history/internal/util"
 )
-
-var (
-	allDatasets  = []golibzfs.Dataset{}
-	AllSnapshots = map[string][]golibzfs.Dataset{}
-)
-
-func init() {
-	RefreshZfsData()
-}
-
-func RefreshZfsData() {
-	loadDatasets()
-	loadSnapshots(allDatasets)
-}
-
-func loadDatasets() {
-	datasets, err := golibzfs.DatasetOpenAll()
-	if err != nil {
-		logging.Error(err.Error())
-	} else {
-		allDatasets = datasets
-	}
-}
-
-func loadSnapshots(datasets []golibzfs.Dataset) {
-	for _, dataset := range datasets {
-		nameProperty := dataset.Properties[golibzfs.DatasetPropName]
-		//mountPointProperty := dataset.Properties[golibzfs.DatasetPropMountpoint]
-		snapshots, err := dataset.Snapshots()
-		if err != nil {
-			logging.Error(err.Error())
-		} else {
-			AllSnapshots[nameProperty.Value] = snapshots
-		}
-		loadSnapshots(dataset.Children)
-	}
-}
 
 type Dataset struct {
 	Path          string
 	HiddenZfsPath string
 	zfsData       *gozfs.Dataset
 	rawDataset    *golibzfs.Dataset
-}
-
-type ZfsData struct {
-	Name          string
-	Origin        string
-	Used          uint64
-	Avail         uint64
-	Mountpoint    string
-	Compression   string
-	Type          string
-	Written       uint64
-	Volsize       uint64
-	Usedbydataset uint64
-	Logicalused   uint64
-	Quota         uint64
 }
 
 func NewDataset(path string, hiddenZfsPath string) (*Dataset, error) {
@@ -272,33 +219,4 @@ func (dataset *Dataset) GetOrigin() string {
 		}
 	}
 	return ""
-}
-
-func findDataset(datasets []golibzfs.Dataset, path string) *golibzfs.Dataset {
-	for _, dataset := range datasets {
-		mountPoint := dataset.Properties[golibzfs.DatasetPropMountpoint]
-		if mountPoint.Value == path {
-			return &dataset
-		}
-		dataset := findDataset(dataset.Children, path)
-		if dataset != nil {
-			return dataset
-		}
-	}
-	return nil
-}
-
-func findSnapshot(snapshots []golibzfs.Dataset, name string) *golibzfs.Dataset {
-	for _, snapshot := range snapshots {
-		nameProperty := snapshot.Properties[golibzfs.DatasetPropName]
-		currentName := strings.Split(nameProperty.Value, "@")[1]
-		if currentName == name {
-			return &snapshot
-		}
-		snapshot := findSnapshot(snapshot.Children, name)
-		if snapshot != nil {
-			return snapshot
-		}
-	}
-	return nil
 }
