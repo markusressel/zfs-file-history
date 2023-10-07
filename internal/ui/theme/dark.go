b1d783e9a029a6f57bf14185d5afdbf977dd6c51
Markus Ressel <mail@markusressel.de>
2023-10-07 15:24:14 +0200

    modify color scheme

--- internal/ui/theme/dark.go
+++ internal/ui/theme/dark.go
@@ -1,7 +1,6 @@
 package theme
 
 import (
-	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
@@ -12,7 +11,9 @@ var (
 
 	OnPrimaryColor = tcell.ColorWhite
 	OnSecondary    = tcell.ColorBlack
+)
 
+var (
 	Colors = Color{
 		Header: HeaderColors{
 			NameBackground: PrimaryColor,
@@ -62,85 +63,3 @@ var (
 		},
 	}
 )
-
-type HeaderColors struct {
-	Name           tcell.Color
-	NameBackground tcell.Color
-
-	PageIndicator           tcell.Color
-	PageIndicatorBackground tcell.Color
-
-	UpdateInterval           tcell.Color
-	UpdateIntervalBackground tcell.Color
-
-	Version           tcell.Color
-	VersionBackground tcell.Color
-}
-
-type FileBrowserColors struct {
-	Table FileBrowserTableColors
-}
-
-type FileBrowserTableColors struct {
-	State FileBrowserTableStatusColors
-}
-
-type FileBrowserTableStatusColors struct {
-	Unknown  tcell.Color
-	Modified tcell.Color
-	Added    tcell.Color
-	Deleted  tcell.Color
-	Equal    tcell.Color
-}
-
-type SnapshotBrowserColors struct {
-	Table SnapshotBrowserTableColors
-}
-
-type SnapshotBrowserTableColors struct {
-	State SnapshotBrowserTableStatusColors
-}
-
-type SnapshotBrowserTableStatusColors struct {
-	Unknown  tcell.Color
-	Modified tcell.Color
-	Added    tcell.Color
-	Deleted  tcell.Color
-	Equal    tcell.Color
-}
-
-type DialogColors struct {
-	Border tcell.Color
-}
-
-type StyleStruct struct {
-	Layout LayoutStyle
-}
-
-type LayoutStyle struct {
-	TitleAlign       int
-	DialogTitleAlign int
-}
-
-type Color struct {
-	Header          HeaderColors
-	Dialog          DialogColors
-	FileBrowser     FileBrowserColors
-	SnapshotBrowser SnapshotBrowserColors
-	Layout          LayoutColors
-}
-
-type LayoutColors struct {
-	Border tcell.Color
-	Title  tcell.Color
-	Table  LayoutTableColors
-}
-
-type LayoutTableColors struct {
-	Header tcell.Color
-}
-
-func CreateTitleText(text string) string {
-	titleText := fmt.Sprintf(" %s ", text)
-	return titleText
-}
