de4665dcd0e0b4df79a6deac095a95a8710fdc78
Markus Ressel <mail@markusressel.de>
2026-07-05 02:08:06 +0200

    Add console output for successful sudoers rule installation

--- cmd/setup.go
+++ cmd/setup.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"zfs-file-history/internal/logging"
 
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
@@ -39,6 +40,7 @@ You MUST run this command with sudo:
 		}
 
 		logging.Info("Successfully installed sudoers rule for user %s at %s", targetUser, filePath)
+		pterm.Print("Successfully installed sudoers rule for user %s at %s\n", targetUser, filePath)
 	},
 }
 
